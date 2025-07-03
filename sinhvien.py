91666c765f534dfceb8f34cf540c479964393dba
Nguyễn Hữu Nghĩa <94152859+huunghia040603@users.noreply.github.com>
2025-07-03 08:25:31 +0700

    up

---
+++ sinhvien.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, redirect, url_for, session, flash, request
+
+sinhvien = Blueprint('sinhvien', __name__, url_prefix='/sinhvien')
+
+
+@sinhvien.route('/dichvu')
+def dichvu():
+   
+    return render_template('page_sinh_vien/dichvusv.html')
+
+@sinhvien.route('')
+def tongquan():
+   
+    return render_template('page_sinh_vien/tongquan.html')
+
+@sinhvien.route('/taikhoan')
+def taikhoan():
+   
+    return render_template('page_sinh_vien/taikhoan.html')
+
+
+
+
+
